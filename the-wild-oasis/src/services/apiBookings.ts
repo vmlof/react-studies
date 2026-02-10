2423115d1e7f5301bf8a32cfc12be06936c29a92
Paulo Vitor <vitormelo.caridade@outlook.com>
2026-02-10 20:10:07 -0300

    computing recent bookings and stays

--- the-wild-oasis/src/services/apiBookings.ts
+++ the-wild-oasis/src/services/apiBookings.ts
@@ -98,6 +98,7 @@ export async function getBooking(id: number) {
 }
 
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
+// date: ISOString
 export async function getBookingsAfterDate(date: string) {
   const { data, error } = await supabase
     .from("bookings")
