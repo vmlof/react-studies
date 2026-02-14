601db45743b7cba7cb6a8694df99a44a2ce1ffa5
Paulo Vitor <vitormelo.caridade@outlook.com>
2026-02-14 16:13:30 -0300

    building the home page

--- the-wild-oasis-website/app/layout.tsx
+++ the-wild-oasis-website/app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from "next";
-import Navigation from "@/app/_components/Navigation";
-import Logo from "@/app/_components/Logo";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: {
@@ -19,13 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100">
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        {children}
-        <footer>Copyright &copy; by The Wild Oasis</footer>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
