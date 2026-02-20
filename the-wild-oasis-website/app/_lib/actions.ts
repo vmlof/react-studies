03e5189e5b6fe4b1623c42ea1f5f6a1a264cc20c
Paulo Vitor <142511739+vmlof@users.noreply.github.com>
2026-02-20 17:07:31 -0300

    sign in import fix

--- the-wild-oasis-website/app/_lib/actions.ts
+++ the-wild-oasis-website/app/_lib/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { signIn } from "next-auth/react";
+import { signIn } from "./auth";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
